b1b31d939364494bc0172a9c7835e817bafe641b
Grégory FAZZINO <gregory.fazzino@atos.net>
2022-12-06 12:21:25 +0100

    first steps day 2

--- day2.js
+++ day2.js
@@ -5,6 +5,18 @@ const fs = require('fs');
 const dataset = fs.readFileSync(sourceFile, 'utf8');
 const parsedDataset = dataset.split(/\r?\n/);
 
+//1 for X
+//2 For Y
+//3 for Z
+
+//Lost ==> 0
+// draw (equal) ==> 3
+// win ==> 6
+
+parsedDataset.forEach(line => {
+    console.log(line)
+})
 
 
-console.log("test" + parsedDataset)
+
+
